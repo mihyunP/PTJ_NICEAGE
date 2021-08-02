fc3901c6a1abc4cabe20701e83a576913eb87554
slovecoms <gloveomg@gmail.com>
2021-08-02 22:07:01 +0900

    domain 수정

--- backend/src/main/java/com/ssafy/niceage/Domain/Board/BoardDTO.java
+++ backend/src/main/java/com/ssafy/niceage/Domain/Board/BoardDTO.java
@@ -1,5 +1,31 @@
 package com.ssafy.niceage.Domain.Board;
 
-public class BoardDTO {
+import javax.persistence.*;
 
+import lombok.Data;
+
+@Entity
+@Data
+public class BoardDTO {
+	@Id @GeneratedValue
+	@Column (name = "board_id")
+	private int Id;
+	@Column (name = "board_title")
+    private String Title;
+	@Column (name = "board_contents")
+    private String Contents;
+	@Column (name = "board_date")
+    private String Date;
+	@Column (name = "user_no")
+    private int userNo;
+	public BoardDTO(int id, String title, String contents, String date, int no) {
+		super();
+		Id = id;
+		Title = title;
+		Contents = contents;
+		Date = date;
+		userNo = no;
+	}
+
+	
 }
