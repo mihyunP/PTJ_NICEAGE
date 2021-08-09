84a7fb7e4329eb0ce785eb8faf2a1e3decb6f427
slovecoms <gloveomg@gmail.com>
2021-08-09 13:56:08 +0900

    feature/board/not complete

--- backend/src/main/java/com/ssafy/niceage/Domain/Board/BoardDTO.java
+++ backend/src/main/java/com/ssafy/niceage/Domain/Board/BoardDTO.java
@@ -1,31 +1,50 @@
 package com.ssafy.niceage.Domain.Board;
 
 import java.util.Date;
+import java.util.List;
+
+import com.ssafy.niceage.Controller.Request.BoardRequest;
+import com.ssafy.niceage.Domain.Comment.Comment;
+import com.ssafy.niceage.Domain.User.User;
 
 import io.swagger.annotations.ApiModel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @ApiModel(value = "게시판 DTO", description = "ㅇ")
 public class BoardDTO {
 	private Long boardId;
+	private List<Comment> comments;
 	private String boardTitle;
 	private String boardContents;
 	private Date boardDate;
-	private Long userNo;
+	private User user;
 	
-	
-	public BoardDTO(Board board) {
-		this.boardTitle = board.getBoardTitle();
-		this.boardContents = board.getBoardContents();
+	public class BoardWrite {
 		
-	
 	}
 	
+	@Builder
+	public BoardDTO(BoardRequest request, User user) {
+		this.boardId = request.getBoardId();
+//		this.comments = request.getComments();
+		this.boardTitle = request.getBoardTitle();
+		this.boardContents = request.getBoardContents();
+		this.boardDate = request.getBoardDate();
+		this.user = user;
+	}
+	
+	public Board toEntity() {
+		return Board.builder()
+				.boardId(this.boardId)
+				.boardTitle(this.boardTitle)
+				.boardContents(this.boardContents)
+				.user(this.user)
+				.build();
+	}
 	
 }
--- backend/src/main/java/com/ssafy/niceage/Service/BoardService.java
+++ backend/src/main/java/com/ssafy/niceage/Service/BoardService.java
@@ -1,20 +1,72 @@
 package com.ssafy.niceage.Service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import com.ssafy.niceage.Controller.MainResponse;
 import com.ssafy.niceage.Controller.Request.BoardRequest;
 import com.ssafy.niceage.Domain.Board.Board;
+import com.ssafy.niceage.Domain.Board.BoardDTO;
+import com.ssafy.niceage.Domain.User.User;
 import com.ssafy.niceage.Repository.BoardRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class BoardService {
 	private final BoardRepository boardRepository;
 
-	public Board save(BoardRequest request) {
-//		return boardRepository.save(request);
-		return null;
+	/**
+	 * 게시글 작성
+	 * @param boardDto
+	 * @return
+	 */
+	public Board save(BoardDTO boardDto) {
+		Board board = boardDto.toEntity();
+		return boardRepository.save(board);
 	}
+
+	/**
+	 * 게시글을 읽는데 내가 쓴 게시글인지 다른 사람이 쓴 게시글인지 true, false로 return
+	 * @param boardDto
+	 * @param user
+	 * @return true : 내가 작성한 게시글 / false : 다른 사람이 작성한 게시글
+	 */
+	public boolean checkUserId(Long userNo, Long boardWriterNo) {
+		if (userNo == boardWriterNo) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public Board findById(Long boardId) {
+		return boardRepository.findByBoardId(boardId);
+	}
+
+	public boolean deleteBoard(boolean isCheck, Long boardId) {
+		if (isCheck) {
+			boardRepository.deleteByBoardId(boardId);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public List<Board> findAll() {
+		return boardRepository.findAll();
+	}
+
+//	public Board update(BoardDTO boardDto) {
+//		Board board = boardDto.toEntity();
+//		return boardRepository.save(board);
+//		
+//	}
+
 }
