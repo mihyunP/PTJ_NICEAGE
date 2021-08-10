eb0453d657a5176eac925e9d58ba6635543cdc2b
hyunseungbb <happyman1104@naver.com>
2021-08-10 10:45:33 +0900

    S05P13B202-180 [feat] FE_0809 - 로그인 및 회원가입 백 연결

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,22 +1,35 @@
 // 백엔드 API 요청은 여기서
 import axios from 'axios'
 
-const BASE_URL = '/api/v1'
+// const BASE_URL = '/api/v1'
 const DEFAULT_ACCEPT_TYPE = 'application/json'
 
-axios.defaults.baseURL = BASE_URL
+// axios.defaults.baseURL = BASE_URL
 axios.defaults.headers['Content-Type'] = DEFAULT_ACCEPT_TYPE
 
 export function requestLogin ({ state }, payload) {
   console.log('requestLogin', state, payload)
-  const url = '/auth/login'
+  const url = '/login'
   let body = payload
   return axios.post(url, body)
 }
 
 export function requestSignup ({ state }, payload) {
   console.log('requestSignup', state, payload)
-  const url = '/users'
+  const url = '/user/join'
   let body = payload
   return axios.post(url, body)
+}
+
+export function requestAuthenticationNumber ({ state }, payload) {
+  console.log('requestAuthenticationNumber', state, payload)
+  const url = '/auth'
+  let body = payload
+  return axios.post(url, body)
+}
+
+export function requestConfirmAuthNum ({ state }, param) {
+  console.log('requestConfirmAuthNum', state, param)
+  const url = `/auth/${param.phoneNumber}/${param.authNum}`
+  return axios.get(url)
 }
