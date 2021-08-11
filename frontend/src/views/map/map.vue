59e2f325c5eef48466daefb374bcb8e7c4c66b38
mihyunP <mihyunhaha@naver.com>
2021-08-12 04:32:10 +0900

    [feat] : FE_0812 - map 마커 추가

--- frontend/src/views/map/map.vue
+++ frontend/src/views/map/map.vue
@@ -2,16 +2,30 @@
     <el-row class="map-container">
         <el-col :span="7">
             <el-scrollbar height="650px">
-                <div v-if="!state.dialogVisible">{{ state.dialogVisible }}</div>
-                <div id="range" class="demo">
-        
-    <!-- <div class="n" v-bind:key="seniorName"  v-for=""(seniorName,seniorAddress) in SeniorCenterInfo">{{ seniorName }} : {{ seniorAddress }}</p> -->
-                </div>
+                <!-- <div v-if="!state.dialogVisible">{{ state.dialogVisible }}</div> -->
+                <!-- <div v-if="!state.dialogVisible">{{ state.SeniorCenterInfo[0].seniorId }}</div> -->
+
+                    <table>
+                        <thead></thead>
+                        <tbody>
+                    <tr v-bind:key="s" v-for="s in state.SeniorCenterInfo">
+                    <td>
+                        <img src="https://kr.seaicons.com/wp-content/uploads/2015/06/house-icon.png" alt="My Image" width="100">
+                    </td>
+                    <td>
+                                {{ s.seniorName }}<br>
+                                {{ s.seniorAddress }}
+                    </td>
+                    </tr>
+                        </tbody>
+                         <div @click="$router.go(-1)">
+                        <span class="iconify" data-inline="false" data-icon="akar-icons:arrow-back-thick-fill" style="color: #f88d8d; font-size: 111px;" ></span>
+                        <span class="previouspage">전 페이지로 돌아가기</span>
+                        </div>
+                    </table>
+         
             </el-scrollbar>
-            <div @click="$router.go(-1)">
-                <span class="iconify" data-inline="false" data-icon="akar-icons:arrow-back-thick-fill" style="color: #f88d8d; font-size: 111px;" ></span>
-                <span class="previouspage">전 페이지로 돌아가기</span>
-            </div>
+              
         </el-col>
         <el-col :span="17">
             <div id="map" style="z-index:0"></div>
@@ -20,7 +34,7 @@
 
     <!--모달창 -->
     <el-dialog style="z-index:100"
-    title="Tips"
+    title="{{}}방정보"
     v-model="state.dialogVisible"
     width="30%"
     :before-close="handleClose">
@@ -50,8 +64,9 @@ export default {
     setup() {
         const store = useStore()
         const state = reactive({
+            
             dialogVisible: false,
-            SeniorCenterInfo : {},            
+            SeniorCenterInfo : [],            
         })
 
         const clickDialogVisible = () => {
@@ -59,11 +74,12 @@ export default {
         }
 
         const initMap = () => {
+
             console.log('123',state.dialogVisible)
             console.log(1+" "+window.kakao);
             var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
             mapOption = {
-                center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+                center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표 <= 로그인한 사용자의 위도, 경도를 파라미터로 전달함
                 level: 3 // 지도의 확대 레벨
             };  
 
@@ -77,14 +93,26 @@ export default {
             imageOption = {offset: new window.kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
             // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-            var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-                markerPosition = new window.kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+            var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+                // markerPosition = new window.kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
 
             // 주소-좌표 변환 객체를 생성합니다
             var geocoder = new  window.kakao.maps.services.Geocoder();
+            
+            
+            // 반복문 시작
+            for (var i = 0; i < state.SeniorCenterInfo.length; i++) {
+            //  console.log(state.SeniorCenterInfo.length);
+            // console.log(state.SeniorCenterInfo[i].seniorAddress);
+            // console.log(state.SeniorCenterInfo[i].seniorName);
             // 주소로 좌표를 검색합니다
-            geocoder.addressSearch('서울특별시 종로구 재동 54-1번지', function(result, status) {
-                console.log('ddd', window.document)
+            // var j=0;
+            console.log("bf"+i);
+            let address = state.SeniorCenterInfo[i].seniorAddress;
+            let name = state.SeniorCenterInfo[i].seniorName;
+            geocoder.addressSearch(address, function(result, status) {
+            console.log("af"+i);
+                console.log(name);
                 // 정상적으로 검색이 완료됐으면 
                 if (status ===  window.kakao.maps.services.Status.OK) {
 
@@ -98,24 +126,20 @@ export default {
                         image: markerImage, // 마커이미지 설정
                         clickable: true 
                     });
-                    console.log("c"+coords);
-                    console.log("m"+markerPosition);
-                    console.log('마커', marker.fa)
+                
                     window.myMarker = marker
                     // 마커가 지도 위에 표시되도록 설정합니다.
                     marker.setMap(map);
 
-                    // 인포윈도우로 장소에 대한 설명을 표시합니다
-                    // var infowindow = new  window.kakao.maps.InfoWindow({
-                    //     content: '<div style="width:150px;text-align:center;padding:6px 0;">종로 경로당</div>'
-                    // });
-                    // infowindow.open(map, marker);
                     // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
-                    var iwContent = '<div style="padding:5px;">종로 경로당</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+                    // var iwContent = <div style="padding:5px;">${name}</div>; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+                    
+                    
 
                     // 인포윈도우를 생성합니다
                     var infowindow = new window.kakao.maps.InfoWindow({
-                        content : iwContent
+                        content : `<div style="width:150px;text-align:center;padding:6px 0;">${name}</div>`, // 백틱(`)
+                        // content : iwContent
                     });
 
                     // 마커에 마우스오버 이벤트를 등록합니다
@@ -133,26 +157,27 @@ export default {
                     window.kakao.maps.event.addListener(marker, 'click', function() {
                         marker.fa.addEventListener("click", function() {
                             state.dialogVisible = true
+                            // 클릭한 노인정 SeniorName 출력
                             
                     })
                     });
                 
                 // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
                     map.setCenter(coords);
-                    console.log('12312412421')
                 } 
             });
         }
-
+        }
         onMounted(() => {
             const userId = store.getters['root/getMyId']
             store.dispatch('root/requestSeniorCenterInfo', {userId : userId})
             .then(result => {
-                console.log(result);
+                // console.log(result);
                 state.SeniorCenterInfo = result.data.data;
-            })
-            if (window.kakao && window.kakao.maps) {
+                console.log("p"+" "+state.SeniorCenterInfo);
+                      if (window.kakao && window.kakao.maps) {
                 console.log(window.kakao);
+                console.log("len"+" "+state.SeniorCenterInfo.length);
                 initMap();
             } else {
                 const script = document.createElement('script');
@@ -160,9 +185,22 @@ export default {
                 script.onload = () => window.kakao.maps.load(initMap);
                 script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=7c87159e377575ded609810fe7d11d0a&libraries=services';
                 document.head.appendChild(script);
-            }        
+            }
+            })
+            // if (window.kakao && window.kakao.maps) {
+            //     console.log(window.kakao);
+            //     console.log("len"+" "+state.SeniorCenterInfo.length);
+            //     initMap();
+            // } else {
+            //     const script = document.createElement('script');
+                
+            //     script.onload = () => window.kakao.maps.load(initMap);
+            //     script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=7c87159e377575ded609810fe7d11d0a&libraries=services';
+            //     document.head.appendChild(script);
+            // }        
         })
-        return { state, clickDialogVisible, initMap }
+
+          return{state, clickDialogVisible,initMap}
     },
 
     // mounted() {
@@ -291,7 +329,7 @@ export default {
 
         // }
     // }
-}
+    }
 </script>
 
 <style>
