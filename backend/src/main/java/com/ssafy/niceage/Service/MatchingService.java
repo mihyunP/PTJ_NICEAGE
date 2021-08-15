42b20d5530230ebe083bb98a8d645785b326cc72
slovecoms <gloveomg@gmail.com>
2021-08-15 14:58:19 +0900

    feature/matching/코드다듬기
    :q

--- backend/src/main/java/com/ssafy/niceage/Service/MatchingService.java
+++ backend/src/main/java/com/ssafy/niceage/Service/MatchingService.java
@@ -1,9 +1,7 @@
 package com.ssafy.niceage.Service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
