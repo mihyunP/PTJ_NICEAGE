c96ae50247715601d8398c243c3edd1dff5f4270
hyunseungbb <happyman1104@naver.com>
2021-08-19 09:42:40 +0900

    [feat] FE_0819 - 스트레칭페이지

--- backend/frontend/vue.config.js
+++ backend/frontend/vue.config.js
@@ -4,11 +4,10 @@ module.exports = {
   outputDir: "../src/main/resources/static",
   indexPath: "../src/main/resources/static/index.html",
   devServer: {
-    port: '8083',
     proxy: 'http://localhost:8080',
   },
   chainWebpack: config =>{
-     const svgRule = config.module.rule("svg");    
+    const svgRule = config.module.rule("svg");    
     svgRule.uses.clear();    
     svgRule.use("vue-svg-loader").loader("vue-svg-loader");
   }
