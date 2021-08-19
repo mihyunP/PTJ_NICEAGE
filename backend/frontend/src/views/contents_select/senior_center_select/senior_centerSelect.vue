6bc4aa1550e25c9b762cf8138d531266aff6f010
홍진빈 <jbhongg@naver.com>
2021-08-19 11:31:24 +0900

    deploy

--- backend/frontend/src/views/contents_select/senior_center_select/senior_centerSelect.vue
+++ backend/frontend/src/views/contents_select/senior_center_select/senior_centerSelect.vue
@@ -53,8 +53,10 @@ import { reactive } from 'vue'
 import { useStore } from 'vuex'
 import axios from 'axios';
 import BackButton from '@/views/components/BackButton'
-const OPENVIDU_SERVER_URL = process.env.VUE_APP_OPENVIDU_SERVER_URL
-const OPENVIDU_SERVER_SECRET = process.env.VUE_APP_OPENVIDU_SERVER_SECRET
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+const OPENVIDU_SERVER_URL = "https://i5b202.p.ssafy.io:4443"
+const OPENVIDU_SERVER_SECRET = "ssafy"
 export default {
   name: 'HealthSelect',
   components: {
